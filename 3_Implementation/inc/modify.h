768d73f0bcb127b1d0c9557ba8595c63775662d8
athulya43 <74306039+athulya43@users.noreply.github.com>
2021-04-16 11:09:58 +0530

    Update and rename modify to modify.h

---
+++ 3_Implementation/inc/modify.h
@@ -0,0 +1,19 @@
+{
+printf("Enter the employee name to modify: ");
+                scanf("%s", empname);
+                rewind(fp);
+                while(fread(&e,recsize,1,fp)==1)  /// fetch all record from file
+                {
+                    if(strcmp(e.name,empname) == 0)  ///if entered name matches with that in file
+                    {
+                        printf("\nEnter new name,age,address and bs: ");
+                        scanf("%s%d%s%f",e.name,&e.age,&e.address,&e.bs);
+                        fseek(fp,-recsize,SEEK_CUR); /// move the cursor 1 step back from current position
+                        fwrite(&e,recsize,1,fp); /// override the record
+                        break;
+                    }
+                }
+                printf("\nModify another record(y/n)");
+                fflush(stdin);
+                another = getche();
+ }
