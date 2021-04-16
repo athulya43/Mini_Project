7cec59c7b50ccb56448f6de9dec7ad04d43f32d0
athulya43 <74306039+athulya43@users.noreply.github.com>
2021-04-16 12:35:01 +0530

    Update add details.h

--- 3_Implementation/inc/add details.h
+++ 3_Implementation/inc/add details.h
@@ -1,24 +1,6 @@
- {
-   
-            system("cls");
-            fseek(fp,0,SEEK_END); /// search the file and move cursor to end of the file
-            /// here 0 indicates moving 0 distance from the end of the file
+#ifndef __ADDRECORD_H__
+#define __ADDRECORD_H__
 
-            another = 'y';
-            while(another == 'y')  /// if user want to add another record
-            {
-                printf("\nEnter name: ");
-                scanf("%s",e.name);
-                printf("\nEnter age: ");
-                scanf("%d", &e.age);
-                printf("\nEnter Address:");
-                scanf("%s",e.address);
-                printf("\nEnter basic salary: ");
-                scanf("%f", &e.bs);
+int addrecord(FILE *fp,struct employee *emp,int temp);
 
-                fwrite(&e,recsize,1,fp); /// write the record in the file
-
-                printf("\nAdd another record(y/n) ");
-                fflush(stdin);
-                another = getche();
-            }
+#endif  
