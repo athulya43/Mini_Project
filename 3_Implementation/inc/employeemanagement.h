8edd47dd2d87aba28246f2091f6aff62d63ce821
athulya43 <74306039+athulya43@users.noreply.github.com>
2021-04-16 12:32:49 +0530

    Create employeemanagement.h

---
+++ 3_Implementation/inc/employeemanagement.h
@@ -0,0 +1,17 @@
+#ifndef __EMPLOYEEMANAGEMENT_H__
+#define __EMPLOYEEMANAGEMENT_H__
+
+#include "addrecord.h"
+#include "deleterecord.h"
+#include "modifyrecord.h"
+#include "displayrecord.h"
+
+#endif
+
+struct emp
+    {
+        char name[40]; ///name of employee
+        int age; /// age of employee
+        char address[20];//address of employee
+        float bs; /// basic salary of employee
+    };
