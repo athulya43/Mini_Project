c6dbbcb8cf4bbcb396f8404b73a3562bf9b9ec79
athulya43 <74306039+athulya43@users.noreply.github.com>
2021-04-16 12:28:34 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -1,1 +1,20 @@
+#include "unity.h"
+#include "recordmanagement.h"
+int main()
+{
+  fp = fopen("data.txt", "rb+");
 
+	if (fp == NULL) 
+	{
+		fp = fopen("data.txt", "wb+");
+	}
+  
+UNITY_BEGIN();
+
+  RUN_TEST(test_displayrecord);
+  RUN_TEST(test_addrecord);
+  RUN_TEST(test_deleterecord);
+  RUN_TEST(test_modifyrecord);
+
+  return UNITY_END();
+}
